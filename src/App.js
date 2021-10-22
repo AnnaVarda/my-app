aa6a534883186bb7b3457524ddfb74b3432bfe63
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-10-22 17:17:26 +0300

    homepage and navbar vol.1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import "./css/reset.css";
 import "./css/style.css";
 import MenuCart from "./containers/MenuCart";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="container">
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />  
+      </div>    
+      <div className="container">      
       <div id="app-holder">
         <div>    
           <MenuCart />        
         </div>
+      </div>
       </div>
     </div>
   );
