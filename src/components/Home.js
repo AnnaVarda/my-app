d9337a32bc11a412d2d337ef5b85c1bdd6c028de
AnnaVarda <77052038+AnnaVarda@users.noreply.github.com>
2021-11-05 08:35:34 +0200

    Install styled-components, material ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,28 +12,32 @@ function Home() {
             <p>Lorem ipsum...</p>
             <Button>Read more...</Button>
             </Col>
-           <Col lg={6}>
+           <Col>
            <p><Image src="https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg" alt="First dance image" fluid /></p>
-           </Col>          
-             
+           </Col> 
+           </Row>
+
+           <Row>
             <Col lg={6}>
            <p><Image src="https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg" alt="Second dance image" fluid /></p>
            </Col>
-            <Col  lg={6}>
+           <Col lg={6}>
             <h2>Dance Lessons</h2>
             <p>Lorem ipsum...</p>
             <Button>Read more...</Button>
             </Col>
-        
-            <Col  lg={6}>
+            </Row>
+
+            <Row>
+            <Col lg={6}>
             <h2>Fitness Lessons</h2>
             <p>Lorem ipsum...</p>
             <Button>Read more...</Button>
             </Col>
-           <Col  lg={6}>
+           <Col lg={6}>
            <p><Image src="https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg" alt="First dance image" fluid /></p>
            </Col>
-            </Row>
+           </Row>
         </Container>
         </div>
     );
